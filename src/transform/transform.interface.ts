9eb5dba1ef539c3050cd5c4f30a1f08a2e4c2d0d
Yongwoo Jung <stegano@naver.com>
2023-07-23 21:36:07 +0900

    fix(transformer): update `preProcessor` interface and `Preset` description

--- src/transform/transform.interface.ts
+++ src/transform/transform.interface.ts
@@ -1,7 +1,7 @@
 /**
  * Pre-processor
  */
-export type PreProcessor = (data: any) => object;
+export type PreProcessor = (data: string | object) => object;
 
 /**
  * Post-processor
@@ -12,11 +12,30 @@ export type PostProcessor = (content: string, data: Readonly<object>) => string;
  * Preset
  */
 export interface Preset {
+  /**
+   * Pre-processors
+   */
   preProcessors?: PreProcessor[];
+  /**
+   * Post-processors
+   */
   postProcessors?: PostProcessor[];
+  /**
+   * Template file path
+   * [!] If `template` is set, this setting is ignored
+   */
   templateFile?: string;
+  /**
+   * Template string
+   */
   template?: string;
+  /**
+   * Output file information
+   */
   outputFile?: {
+    /**
+     * Output file extension
+     */
     ext: string;
   };
 }
